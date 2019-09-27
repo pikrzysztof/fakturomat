5fed006924eaf16e0e34ee3f3b4b0843cb81e45b
Krzysztof Piecuch <piecuch@protonmail.com>
2019-09-27 08:47:51 +0000

    Dodaj zapamietywanie pozycji

--- faktury/models.py
+++ faktury/models.py
@@ -1,87 +1,118 @@
-from django.db import models
+from decimal import Decimal
+from django.db import models, connection
 import datetime
+import slownie
 
 # FIXME: Fix on_delete relationships?
+def zaokraglij(x):
+        return Decimal(x).quantize(Decimal('0.01'))
 
 class JednostkaMiary(models.Model):
-	nazwa = models.CharField(max_length=30)
+        nazwa = models.CharField(max_length=30)
+        
+        def __str__(self):
+                return self.nazwa
 
-
-	def __str__(self):
-		return self.nazwa
-
-	class Meta:
-		verbose_name_plural = "Jednostki miary"
+        class Meta:
+                verbose_name_plural = "Jednostki miary"
 
 class PrzedmiotUmowy(models.Model):
 
-	nazwa = models.CharField(max_length=300, blank=False, null=False)
-	jm = models.ForeignKey(JednostkaMiary,
-	                       on_delete=models.PROTECT,
-	                       help_text='Jednostka (metry, godziny)',
-	                       null=True)
-	pkwiu = models.CharField(max_length=300, blank=True, null=True)
+        nazwa = models.CharField(max_length=300, blank=False, null=False)
+        jm = models.ForeignKey(JednostkaMiary,
+                               on_delete=models.PROTECT,
+                               help_text='Jednostka (metry, godziny)',
+                               null=True)
+        pkwiu = models.CharField(max_length=300, blank=True, null=True)
 
-	def __str__(self):
-		return f'{self.nazwa} ({self.jm.nazwa})'
+        def __str__(self):
+                return f'{self.nazwa} ({self.jm.nazwa})'
 
-	class Meta:
-		verbose_name_plural = "Przedmioty umowy"
+        class Meta:
+                verbose_name_plural = "Przedmioty umowy"
 
 
 class Firma(models.Model):
-	nazwa = models.CharField(max_length=300)
-	adres_pierwsza_linijka = models.CharField(max_length=40, null=True, blank=True)
-	adres_druga_linijka = models.CharField(max_length=40, null=True, blank=True)
-	adres_trzecia_linijka = models.CharField(max_length=40, null=True, blank=True)
-	adres_czwarta_linijka = models.CharField(max_length=40, null=True, blank=True)
-	nip = models.CharField(max_length=30)
+        nazwa = models.CharField(max_length=300)
+        adres_pierwsza_linijka = models.CharField(max_length=40,
+                                                  null=True, blank=True)
+        adres_druga_linijka = models.CharField(max_length=40,
+                                               null=True, blank=True)
+        adres_trzecia_linijka = models.CharField(max_length=40,
+                                                 null=True, blank=True)
+        adres_czwarta_linijka = models.CharField(max_length=40,
+                                                 null=True, blank=True)
+        nip = models.CharField(max_length=30)
 
-	def __str__(self):
-		return self.nazwa
+        def __str__(self):
+                return self.nazwa
 
-	class Meta:
-		verbose_name_plural = 'Firmy'
+        class Meta:
+                verbose_name_plural = 'Firmy'
 
 
 class FormaPlatnosci(models.Model):
-	nazwa = models.CharField(max_length=30)
+        nazwa = models.CharField(max_length=30)
 
-	def __str__(self):
-		return f'{self.nazwa}'
+        def __str__(self):
+                return f'{self.nazwa}'
 
-	class Meta:
-		verbose_name_plural = 'Formy płatności'
+        class Meta:
+                verbose_name_plural = 'Formy płatności'
 
 class Faktura(models.Model):
-	wystawiajacy = models.ForeignKey(Firma, null=False,
-	                                 on_delete=models.PROTECT,
-	                                 related_name='wystawiajacy_fk')
-	klient = models.ForeignKey(Firma, null=False, on_delete=models.PROTECT,
-	                           related_name='klient_fk')
-	pozycje = models.ManyToManyField(PrzedmiotUmowy)
+        wystawiajacy = models.ForeignKey(Firma, null=False,
+                                         on_delete=models.PROTECT,
+                                         related_name='wystawiajacy_fk')
+        klient = models.ForeignKey(Firma,
+                                   null=False, on_delete=models.PROTECT,
+                                   related_name='klient_fk')
+        pozycje = models.ManyToManyField(PrzedmiotUmowy)
 
-	numer = models.CharField(max_length=40)
+        numer = models.CharField(max_length=40)
 
-	data_sprzedazy = models.DateField(default=datetime.date.today)
-	data_wystawienia = models.DateField(default=datetime.date.today)
-	termin_platnosci = models.DateField(default=datetime.date.today)
-	forma_platnosci = models.ForeignKey(FormaPlatnosci, on_delete=models.PROTECT)
-	oplacone = models.BooleanField(default=False)
+        data_sprzedazy = models.DateField(default=datetime.date.today)
+        data_wystawienia = models.DateField(default=datetime.date.today)
+        termin_platnosci = models.DateField(default=datetime.date.today)
+        forma_platnosci = models.ForeignKey(FormaPlatnosci,
+                                            on_delete=models.PROTECT)
+        oplacone = models.BooleanField(default=False)
 
-	def __str__(self):
-		return f'{self.wystawiajacy} dla {self.klient}'
+        def suma_netto(self):
+                pozycje = IlePozycji.objects.filter(faktura=self)
+                return zaokraglij(sum([poz.netto_calosc() for poz in pozycje]))
 
-	class Meta:
-		verbose_name_plural = "Faktury"
+        def suma_brutto(self):
+                pozycje = IlePozycji.objects.filter(faktura=self)
+                return zaokraglij(sum([poz.brutto_calosc() for poz in pozycje]))
+        def slownie_brutto(self):
+                return slownie.slownie_zl100gr(self.suma_brutto())
+
+        def vat(self):
+                return self.suma_brutto() - self.suma_netto()
+
+        def __str__(self):
+                return f'{self.wystawiajacy} dla {self.klient}'
+
+        class Meta:
+                verbose_name_plural = "Faktury"
 
 class IlePozycji(models.Model):
-	faktura = models.ForeignKey(Faktura, null=False,
-	                            on_delete=models.DO_NOTHING)
-	pozycja = models.ForeignKey(PrzedmiotUmowy, null=False,
-	                            on_delete=models.DO_NOTHING)
-	ile = models.DecimalField(max_digits=20, decimal_places=5,
-	                          null=False)
-	podatek_proc = models.DecimalField(max_digits=5, decimal_places=3,
-	                                   null=False)
-	netto = models.DecimalField(max_digits=20, decimal_places=5)
+        faktura = models.ForeignKey(Faktura, null=False,
+                                    on_delete=models.DO_NOTHING)
+        pozycja = models.ForeignKey(PrzedmiotUmowy, null=False,
+                                    on_delete=models.DO_NOTHING)
+        ile = models.DecimalField(max_digits=20, default=Decimal('1'),
+                                  decimal_places=5, null=False)
+        podatek_proc = models.DecimalField(max_digits=5, decimal_places=3, default=Decimal('8'),
+                                           null=False)
+        netto = models.DecimalField(max_digits=20, default=Decimal('0'), decimal_places=5)
+        wyswietlana_nazwa = models.CharField(max_length=300,
+                                             null=True, blank=True)
+
+        def netto_calosc(self):
+                return zaokraglij(self.netto * self.ile)
+        def brutto_calosc(self):
+                return zaokraglij(self.netto_calosc() * (Decimal('1') + self.podatek_proc * Decimal('0.01')))
+        def podatek_zl(self):
+                return zaokraglij(self.brutto_calosc() - self.netto_calosc())
--- faktury/views.py
+++ faktury/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Faktura, Firma, PrzedmiotUmowy
+from .models import Faktura, Firma, PrzedmiotUmowy, IlePozycji
 from decimal import Decimal
 import slownie
 import os
@@ -10,81 +10,68 @@ import sys
 import subprocess
 import re
 from math import modf
+#from import templatetags import obetnij_zera
+
+def obetnij_zera(num):
+        return num.to_integral() if num == num.to_integral() else num.normalize()
 
 # Create your views here.
 def faktura_dir(id):
         return os.path.join('gen_faktury', str(id))
 
+# wyswietla strone do wypelnienia IlePozycji
 @login_required
 def manage_faktura(request, id_faktury):
-	faktura = get_object_or_404(Faktura, pk=id_faktury)
-	slownik = { 'faktura': faktura}
+        faktura = get_object_or_404(Faktura, pk=id_faktury)
+        ilepoz = []
+        for poz in faktura.pozycje.all():
+                obj, created = IlePozycji.objects.get_or_create(faktura=faktura, pozycja=poz)
+                ilepoz.append(obj or created)
+        slownik = { 'faktura': faktura, 'ilepoz': ilepoz}
+        return render(request, 'formularz.html', slownik)
 
-	return render(request, 'formularz.html', slownik)
-
+# requst to POST z rzeczami wypelnionymi w manage_faktura
 @login_required
 def przygotuj_fakture(request, id_faktury):
-	if not os.path.isdir(faktura_dir(id_faktury)):
-		os.mkdir(faktura_dir(id_faktury))
-	with open('szablon.tex', 'r') as szablon:
-		szablonj2 = jinja2.Template(szablon.read())
-	with open(os.path.join(faktura_dir(id_faktury), 'faktura.tex'), mode='w') as tmpl:
-		faktura = get_object_or_404(Faktura, pk=id_faktury)
-		pozycje = []
-		netto_total = Decimal(0)
-		vat = Decimal(0)
-		zipped = zip(request.POST.getlist('wyswietlana_nazwa'),
-		             request.POST.getlist('ile'),
-		             request.POST.getlist('podatek'),
-		             request.POST.getlist('netto'),
-		             request.POST.getlist('poz_id'))
-		for (nazwa, ile, podatek_proc, netto, id) in zipped:
-			ile = Decimal(ile)
-			przedmiot = get_object_or_404(PrzedmiotUmowy, pk=int(id))
-			netto = Decimal(netto).quantize(Decimal('0.01'))
-			wartosc_netto = (ile * netto).quantize(Decimal('0.01'))
-			podatek_zl = (wartosc_netto * Decimal(podatek_proc) / Decimal(100)).quantize(Decimal('0.01'))
+        with open('szablon.tex', 'r') as szablon:
+                szablonj2 = jinja2.Template(szablon.read())
+        if not os.path.isdir(faktura_dir(id_faktury)):
+                os.mkdir(faktura_dir(id_faktury))
+        # zapisac rzeczy ktore dostalismy z POSTa
+        zipped = zip(request.POST.getlist('wyswietlana_nazwa'),
+                     request.POST.getlist('ile'),
+                     request.POST.getlist('podatek'),
+                     request.POST.getlist('netto'),
+                     request.POST.getlist('poz_id'))
+        faktura = get_object_or_404(Faktura, pk=id_faktury)
+        for (nazwa, ile, podatek_proc, netto, id) in zipped:
+                IlePozycji.objects.filter(pk=id).update(ile=obetnij_zera(Decimal(ile)),
+                                                        podatek_proc=obetnij_zera(Decimal(podatek_proc)),
+                                                        netto=Decimal(netto).quantize(Decimal('0.01')),
+                                                        wyswietlana_nazwa=nazwa)
+        poz = IlePozycji.objects.filter(faktura=faktura)
+        slownik = {'faktura': faktura,
+                   'pozycje': poz}
+        # i dopiero teraz wypelnic ten szablon
+        with open(os.path.join(faktura_dir(id_faktury), 'faktura.tex'), mode='w') as tmpl:
+                tmpl.write(szablonj2.render(slownik))
+        subprocess.check_call(['pdflatex', '-halt-on-error', '-output-directory',  faktura_dir(id_faktury),
+                               os.path.join(faktura_dir(id_faktury), 'faktura.tex')])
 
-			poz = { 'wyswietlana_nazwa': nazwa,
-                                'pkwiu': przedmiot.pkwiu,
-			        'jm': przedmiot.jm.nazwa,
-			        'ile': ile,
-			        'netto_za_sztuke': netto,
-			        'wartosc_netto': wartosc_netto,
-			        'podatek_proc': podatek_proc,
-			        'podatek_zl': podatek_zl,
-			        'brutto': podatek_zl + wartosc_netto,
-			        'nazwa' : przedmiot.nazwa,
-			        }
-			netto_total += wartosc_netto
-			vat += podatek_zl
-			pozycje.append(poz)
-
-		brutto_total = vat + netto_total
-		slownik = {
-			'faktura': faktura,
-			'netto_total': netto_total,
-			'vat': vat,
-			'slownie_brutto': slownie.slownie_zl100gr(brutto_total.quantize(Decimal('0.01'))),
-			'pozycje': pozycje
-		}
-		tmpl.write(szablonj2.render(slownik))
-	subprocess.check_call(['pdflatex', '-halt-on-error', '-output-directory',  faktura_dir(id_faktury),
-	                       os.path.join(faktura_dir(id_faktury), 'faktura.tex')])
-
+# wolane kiedy sie chce zobaczyc pdfa
 @login_required
 def gen_faktura(request, id_faktury):
-	if request.method == 'POST':
-		przygotuj_fakture(request, id_faktury)
-	with open(os.path.join(faktura_dir(id_faktury), 'faktura.pdf'), mode='rb') as faktura:
-		return HttpResponse(faktura.read(), content_type='application/pdf')
+        if request.method == 'POST':
+                przygotuj_fakture(request, id_faktury)
+        with open(os.path.join(faktura_dir(id_faktury), 'faktura.pdf'), mode='rb') as faktura:
+                return HttpResponse(faktura.read(), content_type='application/pdf')
 
 def healthz(_request):
-	return HttpResponse(status=200)
+        return HttpResponse(status=200)
 
 @login_required
 def glowna(_request):
-	return render(_request, 'glowna.html', {'faktury': Faktura.objects.all()})
+        return render(_request, 'glowna.html', {'faktury': Faktura.objects.all()})
 
 
 def tex_escape(text):
