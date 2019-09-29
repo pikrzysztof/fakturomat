39c7609cc162e8dedc8f03fe054240ec2b0b57b4
Krzysztof Piecuch <piecuch@protonmail.com>
2019-09-29 23:24:08 +0000

    Add security headers

--- fakturomat/settings.py
+++ fakturomat/settings.py
@@ -21,6 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 60000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ['DEBUG'] == 'True')
